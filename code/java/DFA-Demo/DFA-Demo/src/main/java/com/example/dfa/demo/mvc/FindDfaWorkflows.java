2147272671063bb589889196bb25232ccffb3834
richard.roda@gmail.com <RichardRoda@private.fake>
2015-04-24 10:47:45 -0400

    Spring MVC set up correctly (hello world style).

--- code/java/DFA-Demo/DFA-Demo/src/main/java/com/example/dfa/demo/mvc/FindDfaWorkflows.java
+++ code/java/DFA-Demo/DFA-Demo/src/main/java/com/example/dfa/demo/mvc/FindDfaWorkflows.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  *
  * @author rodare
  */
-@RequestMapping("findWorkflows")
+@RequestMapping("/findWorkflows")
 @Controller
 public class FindDfaWorkflows {
     @RequestMapping(method=RequestMethod.GET)
